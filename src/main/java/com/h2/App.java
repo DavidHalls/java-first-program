ad22ed3ee5a695e69cec0d937d441a2b2f5fd78d
davidhalls <dhalls@wgu.edu>
2021-11-18 18:57:23 -0700

    v1

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -8,10 +8,21 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
+
+        return number * 2;
+    }
+
+    public static int add(int[] numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
         return -1;
     }
+
 }
